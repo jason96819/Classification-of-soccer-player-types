ebb45cc2a7dfe1f77d4b0709a11637a416f62bf7
Jason Jung <74489776+jason96819@users.noreply.github.com>
2023-08-17 01:07:30 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Custom imports 
 from multipage import MultiPage
-from modules import mainmain, role_desc, chatbot_desc, clustering_desc, image_clf_desc, role_clf_desc, searching_desc
+from modules import 1_mainmain, 2_role_desc, 3_chatbot_desc, 4_clustering_desc, 5_image_clf_desc, 6_role_clf_desc, 7_searching_desc
 
 # Create an instance of the app 
 app = MultiPage()
@@ -11,13 +11,13 @@ app = MultiPage()
 st.title("축구 역할 분류 시스템")
 
 # Add all your applications (pages) here
-app.add_page('Main', mainmain.app)
-app.add_page('Role Description', role_desc.app)
-app.add_page('Chatbot Description', chatbot_desc.app)
-app.add_page('Clustering Description', clustering_desc.app)
-app.add_page('Image Classification Description', image_clf_desc.app)
-app.add_page('Role Classification Description', role_clf_desc.app)
-app.add_page('Player Searching Description', searching_desc.app)
+app.add_page('Main', 1_mainmain.app)
+app.add_page('Role Description', 2_role_desc.app)
+app.add_page('Chatbot Description', 3_chatbot_desc.app)
+app.add_page('Clustering Description', 4_clustering_desc.app)
+app.add_page('Image Classification Description', 5_image_clf_desc.app)
+app.add_page('Role Classification Description', 6_role_clf_desc.app)
+app.add_page('Player Searching Description', 7_searching_desc.app)
 
 # The main app
 app.run()
